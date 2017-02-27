b10c679c310b76fb1159ca6e4810417b2634c0fe
Helena Kirk <helenalkirk@gmail.com>
2017-02-27 16:51:25 +0000

    adds review.css file loactions.js file and review.html file

---
+++ locations.js
@@ -0,0 +1,25 @@
+var locations=new Array(
+	{
+	'name': 'Chapters Blackheath',
+	'address': '43-45 Montpelier Vale, Blackheath Village, London SE3 OTJ',
+	'lat': 51.468161,
+	'lng': 0.008108,
+	'url': 'http://www.chaptersblackheath.com/',
+	},
+
+	{
+	'name': 'The Guildford Arms',
+	'address': '55 Guildford Grove, London, SE10 8JY',
+	'lat': 51.474556,
+	'lng': -0.014889,
+	'url': 'http://www.theguildfordarms.co.uk/',
+	},
+
+	{
+	'name': 'The Hare & Billet',
+	'address': '1A Hare & Billet Road, Blackheath, London, SE3 0QJ',
+	'lat': 51.468158,
+	'lng': 0.00369,
+	'url': 'http://www.hareandbillet.com/',
+	}
+	);
